7567a511904c0969873b50ddbb02b03cac6e24d4
Sujay Jayaram <sujay_jayaram@hotmail.com>
2020-04-14 11:47:18 +0100

    React  Data and Router Demo

--- lang-javascript/react/router-demo/src/App.js
+++ lang-javascript/react/router-demo/src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import Nav from './Nav';
+import Shop from './Shop';
+import About from './About';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { useState } from 'react';
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // USE OF REACT HOOKS - https://reactjs.org/docs/hooks-state.html
+    // **** HOOKS DON'T WORK INSIDE CLASSES
+    // Declare a new state variable, which we'll call "count"
+    // When calling useState():
+    // RHS is initial value
+    // LSH is a tuple - a variable, and a set() method to change state
+    // which causes the element to be re-rendered.
+    const [count, setCount] = useState(0);
+
+    return (
+        <Router>
+            <div className="App">
+                <Nav></Nav>
+                <Switch>
+                    <Route path="/" exact component={Home}/>
+                    <Route path="/about" component={About}/>
+                    <Route path="/shop" component={Shop}/>
+                </Switch>
+            </div>
+
+            <p>You clicked {count} times</p>
+            <button onClick={() => setCount(count + 1)}>
+                Click me
+            </button>
+
+        </Router>
+    );
 }
+
+// Another way of creating a component
+const Home = () => (
+    <div>
+        <h1>Home Page: React Version = {React.version}</h1>
+    </div>
+)
 
 export default App;
